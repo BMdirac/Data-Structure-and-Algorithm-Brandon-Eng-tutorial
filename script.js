48b97271b34d30906b63cb36c35c4d69e907cefc
BMdirac <busarimoruf20@gmail.com>
2022-09-05 18:26:56 +0100

    new changes

--- script.js
+++ script.js
@@ -1,3 +1,106 @@
+// ******************************************  FINAL LEARNING PHASE **********************************************************************
+
+
+/* 
+'ctrl + back tick' to open new terminal
+then type 'ls' and press enter (to make sure you are in the right path)
+then type 'node script.js' and press enter (to run the js code)
+note that script.js is the name of our file so it depend on the file name.
+or press the run button at the top right corner of the environment (vs code)
+*/
+
+/*********************************************  ARRAYS *******************/
+
+// let myFirstArray = []; // how to initialize an array
+// // push() is used to add data into the array
+// myFirstArray.push(3);
+// myFirstArray.push(4);
+// myFirstArray.push("Beng");
+// myFirstArray.push(true);
+// console.log(myFirstArray.length)
+// console.log(myFirstArray);
+// console.log(myFirstArray[0]); // to access an array we use index ( first index starts at zero)
+// let value = myFirstArray.pop() // to remove the last data of an array (note that pop can be assign to a variable)
+// console.log(myFirstArray.length)
+// console.log(value)
+// console.log(myFirstArray)
+
+// // note that we can use the length of the array to dictate how many times we want the for loop to run
+// for (let i = 0; i < myFirstArray.length; i++) {
+//     console.log("i", i);
+//     console.log("value", myFirstArray[i]);
+// }
+// // note that for loop breaks out rather than displaying undefined value
+
+// // for loop that can be used specifically for arrays
+// // to print out values without caring about what index they were we use the 'for of loop'
+// for (let value of myFirstArray) {
+//     console.log(value);
+// }
+
+/*********************************************  SETS *******************/
+
+// // sets is a powerful tool if you are trying to find unique value
+// let myFirstSet = new Set()
+// myFirstSet.add(3);
+// myFirstSet.add(3);
+// myFirstSet.add(4);
+// myFirstSet.add('Beng');
+// myFirstSet.add(true);
+// console.log(myFirstSet); // 3 is only added once that is the power of set
+
+// // 'delete' is used to remove a data from set
+// myFirstSet.delete('Beng');
+// // 'size' is used to determine the length of data in set
+// console.log(myFirstSet.size)
+
+// // we can also use for of loop here
+// for (let value of myFirstSet) {
+//     console.log(value);
+// }
+
+// // use has() to check if it has a specific data
+// console.log(myFirstSet.has('Beng'));
+// console.log(myFirstSet.has(4));
+
+/*********************************************  MAPS *******************/
+
+/*
+the thing you put into the map is called the key and the thing
+the map split out is called the value
+
+Essentially a map allows you to connect a key to a value
+*/
+
+// let myFirsMap = new Map();
+// // 'set()' is used to add data to map
+// myFirsMap.set("red", 1)
+// myFirsMap.set("blue", 7)
+// console.log(myFirsMap);
+// // 'get()' is used to get a value for a key from a map
+// console.log(myFirsMap.get("blue"))
+// // note that you cannot have multiples of the same key
+// // all of the key have to be unique ( just like all of the data have to be unique in set)
+
+// // we can also use 'size' to determine the size of map
+// console.log(myFirsMap.size);
+
+// // we can also use 'delete' a specific key-value pair from a map
+// console.log(myFirsMap);
+// myFirsMap.delete("red");
+// console.log(myFirsMap);
+
+// // to use 'for of loop' in map we have to use the function keys()
+// for (let key of myFirsMap.keys()) {
+//     console.log("key", key) // to get key
+//     console.log("Value", myFirsMap.get(key)) // to get value
+// }
+
+// // to see if there is an associated key inside a map use has()
+// console.log(myFirsMap.has("blue"))
+// console.log(myFirsMap.has("red"))
+
+
 // ****************************************** LEARNING PHASE 1 **********************************************************************
 
 // QUEUE and STACK are implemented using an array
